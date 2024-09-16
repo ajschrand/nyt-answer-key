d66bd40fe30e083844bd559d318cd5bdb17fef32
Adam Schrand <adam.schrand@gmail.com>
2024-09-16 12:04:45 -0400

    implemented the bitset class for efficient string util methods

--- utils/str_utils.py
+++ utils/str_utils.py
@@ -1,6 +1,5 @@
 from json import load
 
-
 class MenuOption:
     def __init__(self, title, function):
         self.title = title
@@ -56,7 +55,103 @@ class Menu:
         s += "q - Quit"
 
         return s
+    
+    
+class bitset:
+    @staticmethod
+    def get_bit(char):
+        return 1 << (ord(char) - ord("a"))
+    
+    def __init__(self, word="", bits=0):
+        self.bits = 0
+        if word:
+            for char in word:
+                self.bits |= bitset.get_bit(char)
+        elif bits:
+            self.bits = bits
+            
+    def add(self, char):
+        bit = bitset.get_bit(char)
+        self.bits |= bit
+        
+    def remove(self, char):
+        bit = bitset.get_bit(char)
+        if self.bits & bit == 0:
+            raise KeyError(char)
+        
+        self.bits ^= bit
+        
+    def discard(self, char):
+        bit = bitset.get_bit(char)
+        if self.bits & bit == 0:
+            return
+        
+        self.bits ^= bit
+        
+    def clear(self):
+        self.bits = 0
+        
+    def union(self, other):
+        bits = self.bits | other.bits
+        return bitset(bits=bits)
+    
+    def intersection(self, other):
+        bits = self.bits & other.bits
+        return bitset(bits=bits)
+    
+    def difference(self, other):
+        bits = self.bits ^ (self.bits & other.bits)
+        return bitset(bits=bits)
+    
+    def symmetric_difference(self, other):
+        bits = self.bits ^ other.bits
+        return bitset(bits=bits)
+    
+    def isdisjoint(self, other):
+        return self.bits & other.bits == 0
+    
+    def issubset(self, other):
+        return self.bits & other.bits == self.bits
+    
+    __add__ = union
+    __sub__ = difference
+    __and__ = intersection
+    __xor__ = symmetric_difference
+    __or__ = union
+    
+    def __eq__(self, other):
+        return self.bits == other.bits
+    
+    def __hash__(self):
+        return hash(self.bits)
+    
+    def __len__(self):
+        return self.bits.bit_count()
+    
+    def __contains__(self, char):
+        bit = bitset.get_bit(char)
+        return self.bits & bit > 0
+    
+    def __iter__(self):
+        self.index = 0
+        self.cur = self.bits
+        return self
+    
+    def __next__(self):
+        while self.cur:
+            if self.cur & (1 << self.index):
+                char = chr(ord('a') + self.index)
+                self.index += 1
+                return char
+            
+            self.index += 1
+            
+        raise StopIteration
 
+    def __str__(self):
+        letters = ", ".join(letter for letter in self)
+        return "{" + letters + "}"
+            
 
 def get_list_grid(list, size):
     grid = ""
@@ -71,7 +166,7 @@ def get_list_grid(list, size):
 
 
 def word_has_all(word, letters):
-    return len(set(letters) - set(word)) == 0
+    return bitset(letters).issubset(bitset(word))
 
 
 def word_has_only(word, letters):
@@ -83,7 +178,7 @@ def word_has_only(word, letters):
 
 
 def word_has_duplicates(word):
-    seen = set()
+    seen = bitset()
     for letter in word:
         if letter in seen:
             return True
